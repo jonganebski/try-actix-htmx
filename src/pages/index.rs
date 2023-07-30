b75bde4d086e1e5d5686dc87ba763e1f704e26b9
jonganebski <jon.ganebski@gmail.com>
2023-07-30 18:51:47 +0900

    feat: use askama

--- src/pages/index.rs
+++ src/pages/index.rs
@@ -1,13 +1,23 @@
-use actix_web::{
-    http::{header::ContentType, StatusCode},
-    web::Data,
-    HttpRequest, HttpResponse, Result,
-};
+use actix_web::{web::Data, HttpRequest, Responder, Result};
+use actix_web_lab::respond::Html;
+use askama::Template;
 use reqwest::Client;
 
-pub async fn page(_: Data<Client>, req: HttpRequest) -> Result<HttpResponse> {
+#[derive(Template)]
+#[template(path = "index.html")]
+struct UserTemplate<'a> {
+    name: &'a str,
+    text: &'a str,
+}
+
+pub async fn page(_: Data<Client>, req: HttpRequest) -> Result<impl Responder> {
     println!("{req:?}");
-    Ok(HttpResponse::build(StatusCode::OK)
-        .content_type(ContentType::html())
-        .body(include_str!("./index.html")))
+
+    let html = UserTemplate {
+        name: "Name",
+        text: "Text",
+    }
+    .render()
+    .expect("Failed to render");
+    Ok(Html(html))
 }
